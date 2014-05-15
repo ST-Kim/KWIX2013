f0394de04284985a0f92e978aeda79f246245332
solon777 <solon777@naver.com>
2014-05-15 17:53:54 +0900

    android code

---
+++ Test0212-2/src/com/example/test0212_2/OnDataSelectionListener.java
@@ -0,0 +1,13 @@
+package com.example.test0212_2;
+
+import android.view.View;
+import android.widget.AdapterView;
+
+public class OnDataSelectionListener {
+
+	public void onDataSelected(AdapterView parent, View v, int position, long id) {
+		
+		
+	}
+
+}
